73b9604b8b7bb198f6eafe0c591ed832ef12babd
Andy Sisinger <andocmdo@gmail.com>
2018-04-25 19:33:05 -0400

    add first attempt at string output for agent genome

--- rapide.rb
+++ rapide.rb
@@ -2,29 +2,45 @@
 require 'json'
 
 class Agent
+  attr_accessor :buy_genes, :sell_genes, :hold_genes
+
   def initialize(conf)
     # make our genome arrays
-    buy_genes = Array.new
-    sell_genes = Array.new
-    hold_genes = Array.new
+    @buy_genes = Array.new
+    @sell_genes = Array.new
+    @hold_genes = Array.new
 
     # pull in the startingCash amount
     starting_cash = conf["agent"]["startingCash"]
 
     # Now load in the appropriate files/classes for genome
-    conf["agent"]["buyGenes"].each do |classname|
-      buy_genes << Object.const_get(classname).new #does init work???
+    conf["agent"]["buyGenes"].each do |gene_class|
+      @buy_genes << Object.const_get(gene_class).new #does init work???
     end
-    conf["agent"]["sellGenes"].each do |filename|
-      sell_genes << Object.const_get(classname).new
+    conf["agent"]["sellGenes"].each do |gene_class|
+      @sell_genes << Object.const_get(gene_class).new
     end
-    conf["agent"]["holdGenes"].each do |filename|
-      hold_genes << Object.const_get(classname).new
+    conf["agent"]["holdGenes"].each do |gene_class|
+      @hold_genes << Object.const_get(gene_class).new
     end
-  # init end
+  end # init end
+
+  def genes_to_string
+    result = "buy genes:"
+    @buy_genes.each do |gene|
+      result = "#{result} #{gene.class.name}"
+    end
+    result = "#{result}\nsell genes:"
+    @sell_genes.each do |gene|
+      result = "#{result} #{gene.class.name}"
+    end
+    result = "#{result}\nhold genes:"
+    @hold_genes.each do |gene|
+      result = "#{result} #{gene.class.name}"
+    end
+    return result
   end
-# class end
-end
+end # class end
 
 class SmaPercentAbove
 end
@@ -52,7 +68,14 @@ end
 
 # create the population
 population = Array.new
-(0..config["ga"]["populationSize"]).each do |_|
+(0...config["ga"]["populationSize"]).each do |_|
   population << Agent.new(config)
 end
-puts population
+
+population.each do |agent|
+  puts agent.genes_to_string
+end
+
+# set the population parameters
+mutation_rate = config["ga"]["mutationRate"]
+xover_pool_size_ratio = config["ga"]["xoverPoolSizeRatio"]
