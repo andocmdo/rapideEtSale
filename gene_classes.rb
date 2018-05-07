83aba9c82d6ec24fddc2cfce6b44647ba7bef2c7
Andy Sisinger <andocmdo@gmail.com>
2018-05-07 16:42:23 -0400

    implemented calc method for sma percent class

--- gene_classes.rb
+++ gene_classes.rb
@@ -5,27 +5,61 @@ class SmaPercent
     # these class variables are the individual codons/parts of the gene itself
     @codons = Hash.new
     @codons["percent"] = (rand * 2.0) - 1.0       # limit for activation. are we above the x% SMA(interval)?
+    # TODO make this interval limit configurable
     @codons["interval"] = rand(200) + 1 # interval to calculate the SMA, (such as SMA(200) is 200 day moving average)
-    @codons["over_under"] = [true, false].sample  # true is over the percentage, false is under
+    @codons["above"] = [true, false].sample  # true is above x%, false is below
     @codons["weight"] = rand        # weight to assign this gene
   end
 
-  def calc(record)
-    sma_for_interval = 0.0
-    if !record.data.key?("sma")     # if it doesn't yet exists in the records hash
-      record.data["sma"] = Hash.new # put it in there
-    end
-    if record.data["sma"].key?(@codons["interval"])   # then if we already have a value for that interval
-      sma_for_interval = record.data["sma"][@codons["interval"]]  # then get it
+
+  # at first I was thinking that I could add methods needed for the calculations
+  # for each gene here dynamically, but now I'm thinking I will add it to the record
+  # class anyways...
+  def calc(record, current_index)
+    # if method doesn't exist, add it
+    if !record.respond_to?("sma")
+      # open the class again and insert the method
+      class Record
+        def sma(interval)
+          if interval == 0
+            sma_for_interval = avgOHLC
+          elsif @data["sma"].key?(interval) # if it's already been computed
+            return @data["sma"][interval]   # return precomputed value
+          else
+            # calculate it
+            # if the reqested interval is too far behind our list of records
+            # then limit it to the maximum length from our current record index
+            index = $records.index(this)
+            if index < interval
+              interval = index
+            end
+            # here is the calculation part
+            sum = 0.0
+            (0..interval).each do |i|
+              sum += $records[index - i].avgOHLC
+            end
+            return sum / interval
+          end
+        end # end of sma method
+      end # end of class insertion
+    end # end of method check
+
+    # now since we know the record responds to the required methods, we calc the
+    # activation for this gene
+    # right now it's either activated or not, (on/off), maybe in future we can
+    # return adjusted values according to some activation function (sigmoid?)
+    # for how close it is within it's desired bounds.
+    current_percent_from_sma = (record.avgOHLC / record.sma(interval)) - 1.0
+    if @codons["above"]   # if we are checking for above
+      if current_percent_from_sma > @codons["percent"]
+        return @codons["weight"]
+      end
     else
-      sum = 0.0
-      #TODO this will never work, since we will likely need to rely on the order for which these
-      # items are computed. For instance, if this requires the use of avgOHLC, then before using it
-      # we are going to need to be sure it's been computed... hmmmm
-      # might need to track dependencies... or just make a check dependencies method
-      # and list the dependencies in the config file!
-    end
-  end
+      if current_percent_from_sma < @codons["percent"]
+        return @codons["weight"]
+      end
+    end 
+  end # end of calc method
 
   def mutate(rate)        # We mutate each gene component individually
     if rand < rate
