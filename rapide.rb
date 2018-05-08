6a374915f7278439fc47b28047220e5d31d1dbca
Andy Sisinger <andocmdo@gmail.com>
2018-05-07 23:54:40 -0400

    try loading data from file instead of db

--- rapide.rb
+++ rapide.rb
@@ -1,7 +1,6 @@
 ##### Remember: Quick and Dirty this time...
 require 'json'
 #require 'descriptive_statistics'
-
 require_relative 'agent'
 require_relative 'high_scores_and_stats'
 require_relative 'record'
@@ -11,15 +10,20 @@ require_relative 'gene_classes'
 
 ####################### Script runs below here ################
 # load configuration
-if ARGV[0] != nil && ARGV[0].start_with?("{")
-  config = JSON.parse(ARGV[0])
+if ARGV[0] != nil && ARGV[1] != nil
+  config = JSON.parse(File.read(ARGV[0]))
+  records_hash_array = JSON.parse(File.read(ARGV[1]))
 else
   puts "Missing configuration argument. Need JSON string!"
 end
 
 # load the records for the simulation
 # TODO make this a singleton or something, this is trashy
-$records = Record.load_records(config)
+#$records = Record.load_records(config)
+$records = Array.new
+records_hash_array.each do |record_hash|
+  $records << Record.new(record_hash)
+end
 
 # create the population
 population_size = config["ga"]["populationSize"]
