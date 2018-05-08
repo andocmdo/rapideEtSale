e32d567e08a97daf2c26ef869eb36d39f889d7df
Andy Sisinger <andocmdo@gmail.com>
2018-05-07 20:05:53 -0400

    move sma calc to record class and finish calc method for sma

--- gene_classes.rb
+++ gene_classes.rb
@@ -15,41 +15,16 @@ class SmaPercent
   # at first I was thinking that I could add methods needed for the calculations
   # for each gene here dynamically, but now I'm thinking I will add it to the record
   # class anyways...
-  def calc(record, current_index)
-    # if method doesn't exist, add it
-    if !record.respond_to?("sma")
-      # open the class again and insert the method
-      class Record
-        def sma(interval)
-          if interval == 0
-            sma_for_interval = avgOHLC
-          elsif @data["sma"].key?(interval) # if it's already been computed
-            return @data["sma"][interval]   # return precomputed value
-          else
-            # calculate it
-            # if the reqested interval is too far behind our list of records
-            # then limit it to the maximum length from our current record index
-            index = $records.index(this)
-            if index < interval
-              interval = index
-            end
-            # here is the calculation part
-            sum = 0.0
-            (0..interval).each do |i|
-              sum += $records[index - i].avgOHLC
-            end
-            return sum / interval
-          end
-        end # end of sma method
-      end # end of class insertion
-    end # end of method check
 
+  def calc(record)
     # now since we know the record responds to the required methods, we calc the
     # activation for this gene
     # right now it's either activated or not, (on/off), maybe in future we can
     # return adjusted values according to some activation function (sigmoid?)
     # for how close it is within it's desired bounds.
-    current_percent_from_sma = (record.avgOHLC / record.sma(interval)) - 1.0
+    #puts "record.avgOHLC: #{record.avgOHLC}  record.sma(#{@codons["interval"]}): #{record.sma(@codons["interval"])}"
+    current_percent_from_sma = (record.avgOHLC / record.sma(@codons["interval"])) - 1.0
+    #puts "current_percent_from_sma: #{current_percent_from_sma}"
     if @codons["above"]   # if we are checking for above
       if current_percent_from_sma > @codons["percent"]
         return @codons["weight"]
@@ -58,7 +33,8 @@ class SmaPercent
       if current_percent_from_sma < @codons["percent"]
         return @codons["weight"]
       end
-    end 
+    end
+    return 0.0
   end # end of calc method
 
   def mutate(rate)        # We mutate each gene component individually
